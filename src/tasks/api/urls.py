1d99d17c47bcb35603d19af1d03143fed95f7a17
bishan20 <bishanrasaili20@gmail.com>
2025-04-21 19:01:39 -0400

    feat:create APIs to update and delete tasks

--- src/tasks/api/urls.py
+++ src/tasks/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import create_task_api
+from .views import edit_task_api
+from .views import delete_task_api
 
 urlpatterns = [
     path('create/', create_task_api, name='api_create_task'),
+    path('tasks/<int:task_id>/edit/', edit_task_api, name='api_edit_task'),
+    path('delete/<int:task_id>/', delete_task_api, name='api_delete_task'),
+
 ]
--- src/tasks/api/views.py
+++ src/tasks/api/views.py
@@ -2,7 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from tasks.models import Task
 from .serializers import TaskSerializer
+from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -12,3 +14,23 @@ def create_task_api(request):
         serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def edit_task_api(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    serializer = TaskSerializer(task, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_task_api(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id, user=request.user)
+        task.delete()
+        return Response({"message": "Task deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    except Task.DoesNotExist:
+        return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
