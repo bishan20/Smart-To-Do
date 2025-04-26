c2a3da05805cb55c52334bb350d11fef2ea2adb2
bishan20 <bishanrasaili20@gmail.com>
2025-04-26 18:34:28 -0400

    feat:add first_name/last_name in the register page

--- src/tasks/forms.py
+++ src/tasks/forms.py
@@ -3,11 +3,24 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # Capitalize first letter of first and last names
+        user.first_name = self.cleaned_data['first_name'].capitalize()
+        user.last_name = self.cleaned_data['last_name'].capitalize()
+        user.email = self.cleaned_data['email']
+        
+        if commit:
+            user.save()
+        return user
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
