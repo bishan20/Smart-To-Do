f79ac45b80d1ee3ec0d723778d73a22836500909
bishan20 <bishanrasaili20@gmail.com>
2025-04-25 20:57:07 -0400

    feat:edit profile/password feature and toggle dark mode

--- src/tasks/forms.py
+++ src/tasks/forms.py
@@ -8,3 +8,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,14 +1,23 @@
 from django.urls import include, path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),  # List all tasks
     path('add/', views.add_task, name='add_task'),  # Add a new task
     path('edit/<int:task_id>/', views.edit_task, name='edit_task'),  # Edit an existing task
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),  # Delete a task
+
     path('tasks/<int:task_id>/toggle/', views.toggle_complete, name='toggle_complete'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('settings/', views.settings_view, name='settings'),
+    path('settings/profile/', views.profile_view, name='profile'),
+    path('settings/update/', views.update_profile, name='update_profile'),
+
     path('base/', views.base_view, name='base_view'),  # URL to test base.html
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='tasks/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='tasks/password_change_done.html'), name='password_change_done'),
 
     # Authentication URLs
     path('register/', views.register, name='register'),
